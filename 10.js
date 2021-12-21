0828ad9477c9716e59acac76160ab3467bfe9c32
Mattia Rosettin <m.rosettin@donboscosandona.it>
2021-12-21 11:49:19 +0100

    copia esercizio 10

---
+++ 10.js
@@ -0,0 +1,8 @@
+// definire 5 nomi di fiori
+// quindi scriverli tutti
+
+let fiori = ['margherita', 'rosa', 'mughetto', 'primula', 'viola'];
+
+for(let i=fiori.length - 1; i >= 0 ; i--) {
+    console.log(fiori[i]);
+}
