057eb36d6c3d758e7fd14bd7c5b571155e82a685
Mattia Rosettin <m.rosettin@donboscosandona.it>
2022-01-10 11:50:05 +0100

    re

---
+++ 20.js
@@ -0,0 +1,12 @@
+parole = [patata, arancia, pesce,
+          sasso, arancione, mandarino];
+
+
+          let parole2 = [];
+
+for (i = 0; i < parole.length; i++) {
+    if(parole[i].length)            
+
+          }
+
+          console.log(parole2);
